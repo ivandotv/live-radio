221035f63ea8671acbdfdc03c4698374dd7d0919
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-11-07 20:34:11 +0100

    skip caching songinfo and locationinfo requests
    closes #19

--- next.config.js
+++ next.config.js
@@ -2,16 +2,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const runtimeCaching = require('./src/cache')
 
 const nextConfig = {
   pwa: {
     dest: 'public',
     runtimeCaching,
     scope: '/app',
-    disable: process.env.NODE_ENV === 'development'
+    disable: process.env.NODE_ENV !== 'production'
   }
-
   // typescript: {
   //   // !! WARN !!
   //   // TEMP: Remove for production
