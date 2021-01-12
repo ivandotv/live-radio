ccd6b092efa8a53929105c24837365722b7ceff7
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-01-12 22:32:10 +0100

    setup landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Link from 'next/link'
-// @ts-expect-error
-import Banner from '../svg/landing-page.svg'
 import Head from 'next/head'
 import { t } from '@lingui/macro'
 import { PageTitle } from 'components/PageTitle'
@@ -15,15 +13,19 @@ export default function Index() {
         ></link>
       </Head>
       <div className="page-wrap">
-        <PageTitle title={t`Welcome to Next Radio App`} />
-        <h1>NEXT.JS PWA</h1>
+        <PageTitle title={t`Welcome to Live Radio App`} />
+        <h1>Live Radio</h1>
         <div className="banner-wrapper">
-          {/* <img src="/images/landing-page.svg" /> */}
-          <Banner />
+          <img src="/images/speed-sound-note.svg" />
         </div>
-        <Link href="/app">
-          <a className="launch-app">{t`Launch Application`}</a>
-        </Link>
+        <div className="btn-wrap">
+          <Link href="/app">
+            <a className="app-btn">{t`Login or Register`}</a>
+          </Link>
+          <Link href="/app">
+            <a className="app-btn">{t`Anonymous User`}</a>
+          </Link>
+        </div>
         <style jsx>
           {`
             .page-wrap {
@@ -32,19 +34,34 @@ export default function Index() {
               align-items: center;
             }
             h1 {
-              font-size: 2rem;
-              margin-bottom: 0;
+              font-size: 4rem;
+              margin-top: 8px;
+              margin-bottom: 16px;
             }
             .banner-wrapper {
               width: 100%;
               max-width: 500px;
+              display: flex;
+              justify-content: center;
             }
-            .launch-app {
+            .banner-wrapper img {
+              width: 90%;
+              height: auto;
+              margin-bottom: 24px;
+            }
+            .btn-wrap {
+              margin-top: 24px;
+            }
+            .app-btn {
               background-color: blue;
-              border-radius: 10px;
+              border-radius: 6px;
               color: #fff;
-              padding: 20px;
+              padding: 16px;
+              font-size: 0.9rem;
               text-decoration: none;
+            }
+            .app-btn:last-child {
+              margin-left: 8px;
             }
           `}
         </style>
