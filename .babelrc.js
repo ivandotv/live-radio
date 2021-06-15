cdab9e1da07af732c5b236bfbad424965c1c9f3e
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-06-15 22:30:42 +0200

    remove console log for production

--- .babelrc.js
+++ .babelrc.js
@@ -24,9 +24,9 @@ const plugins = [
   ]
 ]
 
-// if (process.env.NODE_ENV === 'production') {
-//   plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }])
-// }
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }])
+}
 module.exports = {
   presets: ['next/babel'],
   plugins
