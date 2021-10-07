ebedb2b7c17183e2c0f05fbfc66f77333449ee4c
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-10-07 16:28:23 +0200

    refactor root store factory

--- src/lib/stores/root-store.ts
+++ src/lib/stores/root-store.ts
@@ -7,6 +7,7 @@ import {
   musicPlayerFactory,
   MusicPlayerStore
 } from 'lib/stores/music-player-store'
+import { isSSR } from 'lib/utils'
 import { enableStaticRendering } from 'mobx-react-lite'
 import { RecentStore, recentStoreFactory } from './recent-store'
 
@@ -15,12 +16,11 @@ enableStaticRendering(typeof window === 'undefined')
 let store: RootStore
 
 export function rootStoreFactory() {
-  const isSSR = typeof window === 'undefined'
-  const _store = store ?? new RootStore()
+  if (isSSR() || !store) {
+    store = new RootStore()
+  }
 
-  if (isSSR) return _store
-
-  return (store = _store)
+  return store
 }
 
 export class RootStore {
