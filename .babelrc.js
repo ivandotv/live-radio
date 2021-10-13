01eb2dd94285a709edb0e4f42d744236ef544cd3
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-10-13 22:07:00 +0200

    remove console warn and error from production builds

--- .babelrc.js
+++ .babelrc.js
@@ -25,7 +25,8 @@ const plugins = [
 ]
 
 if (process.env.NODE_ENV === 'production') {
-  plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }])
+  // plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }])
+  plugins.push(['transform-remove-console'])
 }
 module.exports = {
   presets: ['next/babel'],
