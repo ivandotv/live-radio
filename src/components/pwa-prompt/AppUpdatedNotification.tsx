6c4b1fa1858be143ea2a5197e59ab6e31c82bdab
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-10-13 23:11:52 +0200

    use notistack popup for app update

--- src/components/pwa-prompt/AppUpdatedNotification.tsx
+++ src/components/pwa-prompt/AppUpdatedNotification.tsx
@@ -1,42 +1,24 @@
-import Snackbar from '@material-ui/core/Snackbar'
-import Alert from '@material-ui/lab/Alert'
-import Cookies from 'js-cookie'
-import React, { useEffect } from 'react'
 import { t } from '@lingui/macro'
+import Cookies from 'js-cookie'
+import { useSnackbar } from 'notistack'
+import { useEffect } from 'react'
 
 export function AppUpdatedNotification({ cookieName }: { cookieName: string }) {
-  const [open, setOpen] = React.useState(false)
+  const { enqueueSnackbar } = useSnackbar()
 
   useEffect(() => {
     if (Cookies.get(cookieName)) {
       Cookies.remove(cookieName)
-      setOpen(true)
+      enqueueSnackbar(t`Application updated`, {
+        variant: 'success',
+        autoHideDuration: 2000,
+        anchorOrigin: {
+          vertical: 'bottom',
+          horizontal: 'center'
+        }
+      })
     }
-  }, [cookieName])
+  }, [cookieName, enqueueSnackbar])
 
-  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
-    if (reason === 'clickaway') {
-      return
-    }
-
-    setOpen(false)
-  }
-
-  return (
-    <Snackbar
-      open={open}
-      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      autoHideDuration={5000}
-      onClose={handleClose}
-    >
-      <Alert
-        elevation={6}
-        variant="filled"
-        onClose={handleClose}
-        severity="success"
-      >
-        {t`Application Updated!`}
-      </Alert>
-    </Snackbar>
-  )
+  return null
 }
