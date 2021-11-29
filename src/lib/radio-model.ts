d1a2e548481098459d78388fe56dee6161336bb5
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-11-29 19:57:09 +0100

    fix class model extension

--- src/lib/radio-model.ts
+++ src/lib/radio-model.ts
@@ -1,11 +1,13 @@
 import { Model } from '@fuerte/core'
 import { RadioDTO } from './station-utils'
+import { FavoritesStore } from './stores/favorites-store'
+import { RecentStore } from './stores/recent-store'
 
 export function createRadioModel(data: RadioDTO) {
   return new RadioModel(data)
 }
 
-export class RadioModel extends Model {
+export class RadioModel extends Model<FavoritesStore | RecentStore> {
   static identityKey = 'id'
 
   id: string
