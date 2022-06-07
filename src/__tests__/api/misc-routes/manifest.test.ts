3952fb8a63ad10bd501bad6e0d10f507f769b83f
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-06-07 20:49:25 +0000

    refactor "manifest" tests

--- src/__tests__/api/misc-routes/manifest.test.ts
+++ src/__tests__/api/misc-routes/manifest.test.ts
@@ -7,7 +7,7 @@ const container = createTestContainer()
 const url = '/api/manifest'
 
 describe('Manifest', () => {
-  test('Pick locale based on header', async () => {
+  test('pick locale based on header', async () => {
     const result = await request(withKoaApi(handler(container)))
       .get(url)
       .set('accept-language', 'sr')
@@ -16,7 +16,7 @@ describe('Manifest', () => {
     expect(result.body.start_url).toMatch('/sr/app')
   })
 
-  test('Pick locale based on cookie', async () => {
+  test('pick locale based on cookie', async () => {
     const result = await request(withKoaApi(handler(container)))
       .get(url)
       .set('Cookie', ['NEXT_LOCALE=sr;'])
@@ -25,14 +25,14 @@ describe('Manifest', () => {
     expect(result.body.start_url).toMatch('/sr/app')
   })
 
-  test('Pick default locale when no headers or cookies', async () => {
+  test('pick default locale when no headers or cookies', async () => {
     const result = await request(withKoaApi(handler(container))).get(url)
 
     expect(result.status).toBe(200)
     expect(result.body.start_url).toMatch('/en/app')
   })
 
-  test('Pick default locale when unsupported locale detected', async () => {
+  test('pick default locale when unsupported locale detected', async () => {
     const result = await request(withKoaApi(handler(container)))
       .get(url)
       .set('accept-language', 'fr')
@@ -41,7 +41,7 @@ describe('Manifest', () => {
     expect(result.body.start_url).toMatch('/en/app')
   })
 
-  test('Fallback to cookie when header locale not supported', async () => {
+  test('fallback to cookie when header locale not supported', async () => {
     const result = await request(withKoaApi(handler(container)))
       .get(url)
       .set('accept-language', 'fr')
