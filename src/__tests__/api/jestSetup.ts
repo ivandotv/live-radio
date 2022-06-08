b2976f9685a997d6cdbdc76cd6de48d7678da663
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-06-08 11:06:38 +0000

    refactor manifest test

--- src/__tests__/api/jestSetup.ts
+++
@@ -1,3 +0,0 @@
-import path from 'node:path'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config({ debug: true, path: path.resolve(__dirname, '.env') })
--- src/__tests__/api/misc-routes/manifest.test.ts
+++ src/__tests__/api/misc-routes/manifest.test.ts
@@ -1,23 +1,26 @@
 import { withKoaApi } from 'nextjs-koa-api'
 import { handler } from 'pages/api/[[...routes]]'
 import request from 'supertest'
-import { createTestContainer } from '../../__utils__/utils'
+import { createTestContainer } from '__tests__/__utils__/utils'
 
 const container = createTestContainer()
 const url = '/api/manifest'
+let api: ReturnType<typeof withKoaApi>
 
 describe('Manifest', () => {
+  beforeEach(() => {
+    api = withKoaApi(handler(container))
+  })
+
   test('pick locale based on header', async () => {
-    const result = await request(withKoaApi(handler(container)))
-      .get(url)
-      .set('accept-language', 'sr')
+    const result = await request(api).get(url).set('accept-language', 'sr')
 
     expect(result.status).toBe(200)
     expect(result.body.start_url).toMatch('/sr/app')
   })
 
   test('pick locale based on cookie', async () => {
-    const result = await request(withKoaApi(handler(container)))
+    const result = await request(api)
       .get(url)
       .set('Cookie', ['NEXT_LOCALE=sr;'])
 
@@ -26,23 +29,21 @@ describe('Manifest', () => {
   })
 
   test('pick default locale when no headers or cookies', async () => {
-    const result = await request(withKoaApi(handler(container))).get(url)
+    const result = await request(api).get(url)
 
     expect(result.status).toBe(200)
     expect(result.body.start_url).toMatch('/en/app')
   })
 
   test('pick default locale when unsupported locale detected', async () => {
-    const result = await request(withKoaApi(handler(container)))
-      .get(url)
-      .set('accept-language', 'fr')
+    const result = await request(api).get(url).set('accept-language', 'fr')
 
     expect(result.status).toBe(200)
     expect(result.body.start_url).toMatch('/en/app')
   })
 
   test('fallback to cookie when header locale not supported', async () => {
-    const result = await request(withKoaApi(handler(container)))
+    const result = await request(api)
       .get(url)
       .set('accept-language', 'fr')
       .set('Cookie', ['NEXT_LOCALE=sr;'])
