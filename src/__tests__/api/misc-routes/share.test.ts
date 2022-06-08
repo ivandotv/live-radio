aa27f1d774209fc4efd4f9dee4436e0018414dea
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-06-08 15:30:45 +0000

    write share api tests

---
+++ src/__tests__/api/misc-routes/share.test.ts
@@ -0,0 +1,58 @@
+import { withKoaApi } from 'nextjs-koa-api'
+import { handler } from 'pages/api/[[...routes]]'
+import request from 'supertest'
+import { createTestContainer } from '__tests__/__utils__/utils'
+
+const container = createTestContainer()
+const url = '/api/share'
+let api: ReturnType<typeof withKoaApi>
+
+describe('/api/share', () => {
+  beforeEach(() => {
+    api = withKoaApi(handler(container))
+  })
+
+  test('redirect', async () => {
+    const stationId = '123'
+
+    const result = await request(api).get(url).query({ play: stationId })
+
+    expect(result.status).toBe(302)
+    expect(result.header.location).toBe(`/en/app?play=${stationId}`)
+    expect(result.body).toEqual({})
+  })
+
+  test('redirect with appropriate locale', async () => {
+    const stationId = '123'
+
+    const result = await request(api)
+      .get(url)
+      .set('accept-language', 'sr')
+      .query({ play: stationId })
+
+    expect(result.status).toBe(302)
+    expect(result.header.location).toBe(`/sr/app?play=${stationId}`)
+    expect(result.body).toEqual({})
+  })
+
+  test('if locale is not supported redirect to default locale', async () => {
+    const stationId = '123'
+
+    const result = await request(api)
+      .get(url)
+      .set('accept-language', 'fr')
+      .query({ play: stationId })
+
+    expect(result.status).toBe(302)
+    expect(result.header.location).toBe(`/en/app?play=${stationId}`)
+    expect(result.body).toEqual({})
+  })
+
+  test('if query is missing, redirect to the root of the site', async () => {
+    const result = await request(api).get(url).set('accept-language', 'sr')
+
+    expect(result.status).toBe(302)
+    expect(result.header.location).toBe('/')
+    expect(result.body).toEqual({})
+  })
+})
