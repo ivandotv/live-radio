c97e67daf15247f81260a102f610a3d1400dfa56
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-07-05 18:48:02 +0200

    refactor react error handler

--- src/components/WindowErrorHandler.tsx
+++ src/components/WindowErrorHandler.tsx
@@ -6,20 +6,32 @@ import { useErrorHandler } from 'react-error-boundary'
   If we run "handlerError" it will unmount the whole react tree
   and show a fallback component to reload the app
   TODO - filter which errors should force complete app reload
-  TODO - log errors
 */
+
 export function WindowErrorHandler() {
   const handleError = useErrorHandler()
 
   useEffect(() => {
-    window.addEventListener('unhandledrejection', (_event) => {
-      // handleError(event)
-      logger.warn('unhandledrejection', _event)
-    })
-    window.addEventListener('error', (_event) => {
-      // handleError(event)
-      logger.warn(_event)
-    })
+    const rejectionCb = (event: PromiseRejectionEvent) => {
+      logger.warn('unhandled rejection', event)
+      handleError(event)
+    }
+
+    const errorCb = (event: ErrorEvent) => {
+      // ignore resize observer errors - (third party package - react virtuoso)
+      if (event.message?.search(/resizeobserver/i) === -1) {
+        logger.warn('unhandled error', event)
+        handleError(event)
+      }
+    }
+
+    window.addEventListener('unhandledrejection', rejectionCb)
+    window.addEventListener('error', errorCb)
+
+    return () => {
+      window.removeEventListener('unhandledrejection', rejectionCb)
+      window.removeEventListener('error', errorCb)
+    }
   }, [handleError])
 
   return null
