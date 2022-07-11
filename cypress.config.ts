b329ebf7d62e589e7303c496b7546ca3c9ebf01a
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-07-11 23:53:31 +0200

    wip 11

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,8 @@ console.log('Cypress config file ', process.env.NEXT_PUBLIC_VERCEL_URL)
 export default defineConfig({
   video: false,
   e2e: {
-    baseUrl: process.env.NEXT_PUBLIC_VERCEL_URL,
-    // baseUrl: 'http://localhost:3000',
+    // baseUrl: process.env.NEXT_PUBLIC_VERCEL_URL,
+    baseUrl: 'http://localhost:3000',
     chromeWebSecurity: false
   }
 })
