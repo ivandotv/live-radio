006792fd4681fc06ab8f01aea94ece498eb80d04
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-07-11 23:14:39 +0200

    wip waith on

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ console.log('Cypress config file ', process.env.NEXT_PUBLIC_VERCEL_URL)
 export default defineConfig({
   video: false,
   e2e: {
-    baseUrl: process.env.NEXT_PUBLIC_VERCEL_URL
+    // baseUrl: process.env.NEXT_PUBLIC_VERCEL_URL
+    baseUrl: 'http://localhost:3000'
   }
 })
