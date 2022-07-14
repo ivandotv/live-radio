5831294d62055ba955ecf9f3291a02ee6dd22ca9
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-07-14 20:50:50 +0200

    ci: move e2e script to dedicated directory

---
+++ scripts-ci/test-e2e.mjs
@@ -0,0 +1,85 @@
+import * as dotenv from 'dotenv'
+import { type } from 'node:os'
+import path, { dirname } from 'node:path'
+import { GenericContainer } from 'testcontainers'
+import { fileURLToPath } from 'url'
+import { $ } from 'zx'
+import 'zx/globals'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+;(async function main() {
+  //disable automatic cleanup of test containers
+  process.env.TESTCONTAINERS_RYUK_DISABLED = true
+
+  let mongoContainer
+  let server
+  let hasError = false
+  try {
+    dotenv.config({
+      debug: true,
+      override: true,
+      path: path.resolve(__dirname, '../', '.env.test')
+    })
+
+    mongoContainer = await startMongo()
+
+    process.env.NODE_ENV = 'test'
+    process.env.E2E = 'true'
+
+    server = $`pnpm start`
+
+    await $`pnpm cypress run --browser chrome`
+  } catch (e) {
+    hasError = true
+    console.log(e)
+  } finally {
+    if (mongoContainer) {
+      console.log('stopping mongo')
+      await mongoContainer.stop()
+      console.log('mongo stopped')
+    }
+    if (server) {
+      console.log('stopping server')
+      await server.kill()
+      console.log('server stopped')
+    }
+
+    process.exit(hasError ? 1 : 0)
+  }
+})()
+
+async function startMongo() {
+  const remoteContainers =
+    Boolean(process.env.REMOTE_CONTAINERS) || Boolean(process.env.CODESPACES)
+  const newtworkAlias = 'mongo-test-db'
+
+  const mongoContainer = new GenericContainer('mongo:5.0.8').withExposedPorts(
+    27017
+  )
+
+  console.log('using remote containers: ', remoteContainers)
+
+  if (type() === 'Linux') {
+    console.log('mongo: using tmpfs mount')
+    mongoContainer.withTmpFs({ '/data/db': '' })
+  }
+  if (remoteContainers) {
+    mongoContainer
+      .withNetworkMode('development')
+      .withNetworkAliases(newtworkAlias)
+  }
+
+  const mongoStarted = await mongoContainer.start()
+
+  const host = remoteContainers ? newtworkAlias : 'localhost'
+
+  const port = remoteContainers ? '27017' : mongoStarted.getMappedPort(27017)
+
+  process.env.MONGO_DB_URI = `mongodb://${host}:${port}`
+
+  console.log('mongo db started')
+  console.log('MONGO_DB_URI: ', process.env.MONGO_DB_URI)
+
+  return mongoStarted
+}
